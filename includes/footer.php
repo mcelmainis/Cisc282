c1830c3ca80f5de0f0f22a7854475237c8c05c27
mcelmainis <34386684+mcelmainis@users.noreply.github.com>
2017-12-11 15:55:54 -0500

    foot notes for assn2 page

---
+++ includes/footer.php
@@ -0,0 +1,11 @@
+<html>
+<body>
+
+<footer>
+&copy; Malcolm Celmainis
+Contact info: 604-986-4863 >> info@giftworksboutique.com
+Find us on Facebook or Instagram!
+</footer>
+
+</body>
+</html>
---
+++ includes/hourTable.php
@@ -0,0 +1,22 @@
+	<html>
+	<body>
+	
+	
+	<table id="hoursTable"> 
+		<thead>
+			<th colspan="2">Store Hours</th>
+		</thead>
+		<tbody>
+		<tr>	<td> Monday - saturday</td><td>Sunday & Holidays</td>
+		</tr>
+							
+		<tr>
+			<td>9:30 to 6:00</td>
+			<td>11:00 to 5:00</td>
+		</tr>
+	</table>
+</block>
+<hr/>
+
+	</body>
+	</html>
